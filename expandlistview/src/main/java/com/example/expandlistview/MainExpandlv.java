229761a622c18f499b5b3653d5e8aa46ca4460d2
huynhtran1509 <huynhtran1509@gmal.com>
2017-04-05 15:39:37 +0700

    Initial commit

---
+++ expandlistview/src/main/java/com/example/expandlistview/MainExpandlv.java
@@ -0,0 +1,13 @@
+package com.example.expandlistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainExpandlv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_expandlv);
+    }
+}
